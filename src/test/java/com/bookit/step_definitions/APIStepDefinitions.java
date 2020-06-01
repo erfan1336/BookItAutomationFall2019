14afb0d4dcb857c1970c288f5406e0c6b904040b
erfan1336 <erfan1336@gmail.com>
2020-06-01 14:51:16 -0700

    step definitions added

--- src/test/java/com/bookit/step_definitions/APIStepDefinitions.java
+++ src/test/java/com/bookit/step_definitions/APIStepDefinitions.java
@@ -1,5 +1,6 @@
 package com.bookit.step_definitions;
 
+import com.bookit.utilities.APIUtilities;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -7,63 +8,64 @@ import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
 
 public class APIStepDefinitions {
 
-    private RequestSpecification requestSpecification;
-    private Response response;
-    private String token;
-    private JsonPath jsonPath;
-    private ContentType contentType;
+    private RequestSpecification requestSpecification; //this is what we put in given
+    private Response response; //this is where we store response data
+    private String token; //this is what we use for authentication
+    private JsonPath jsonPath;  //this is where we store JSON body
+    private ContentType contentType;  //this is what we use to setup content type
 
 
+    //Given authorization token is provided for "teacher"
     @Given("authorization token is provided for {string}")
-    public void authorization_token_is_provided_for(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void authorization_token_is_provided_for(String role) {
+            token = APIUtilities.getToken(role);
     }
 
     @Given("user accepts content type as {string}")
     public void user_accepts_content_type_as(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+            if (string.toLowerCase().contains("json")){
+                contentType = contentType.JSON;
+            }else if (string.toLowerCase().contains("xml")){
+                contentType = contentType.XML;
+            }else if (string.toLowerCase().contains("html")){
+                contentType = contentType.HTML;
+            }
+
     }
 
     @When("user sends GET request to {string}")
-    public void user_sends_GET_request_to(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_sends_GET_request_to(String path) {
+            response = given().accept(contentType).auth().oauth2(token).when().get(path).prettyPeek();
     }
 
     @Then("user should be able to see {int} rooms")
-    public void user_should_be_able_to_see_rooms(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_should_be_able_to_see_rooms(int expectedNumberOfRooms) {
+        List<Object> rooms = response.jsonPath().get();
+        Assert.assertEquals(expectedNumberOfRooms,rooms.size());
     }
 
     @Then("user verifies that response status code is {int}")
-    public void user_verifies_that_response_status_code_is(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_verifies_that_response_status_code_is(int expectedStatusCode) {
+        Assert.assertEquals(expectedStatusCode,response.statusCode());
     }
 
     @Then("user should be able to see all room names")
     public void user_should_be_able_to_see_all_room_names() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
 
     @Then("user payload contains following room names:")
-    public void user_payload_contains_following_room_names(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
+    public void user_payload_contains_following_room_names(List<String> dataTable) {
+
     }
 
 }
