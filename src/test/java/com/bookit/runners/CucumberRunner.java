7954ab284b3a9abb43c2ebef928cddef90f994ea
erfan1336 <erfan1336@gmail.com>
2020-06-01 15:48:33 -0700

    step definitions added

--- src/test/java/com/bookit/runners/CucumberRunner.java
+++ src/test/java/com/bookit/runners/CucumberRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
                 "json:target/cucumber.json"
         },
         tags = "@api",
-        dryRun = true
+        dryRun = false
 )
 
 public class CucumberRunner {
--- src/test/java/com/bookit/step_definitions/APIStepDefinitions.java
+++ src/test/java/com/bookit/step_definitions/APIStepDefinitions.java
@@ -1,5 +1,6 @@
 package com.bookit.step_definitions;
 
+import com.bookit.pojos.Room;
 import com.bookit.utilities.APIUtilities;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -8,11 +9,14 @@ import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matcher;
+import org.hamcrest.MatcherAssert;
 import org.junit.Assert;
 
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class APIStepDefinitions {
 
@@ -59,13 +63,18 @@ public class APIStepDefinitions {
 
     @Then("user should be able to see all room names")
     public void user_should_be_able_to_see_all_room_names() {
-
+        List<Room> rooms = response.jsonPath().getList("",Room.class);
+//      rooms.forEach(room -> System.out.println(room.getName()));
+        for (Room room: rooms){
+            System.out.println(room.getName());
+        }
     }
-
 
     @Then("user payload contains following room names:")
     public void user_payload_contains_following_room_names(List<String> dataTable) {
-
+        List<String> roomNames = response.jsonPath().getList("name");
+        Assert.assertTrue(roomNames.containsAll(dataTable));
+        MatcherAssert.assertThat(roomNames,hasItem(in(dataTable)));
     }
 
 }
